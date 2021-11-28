372deaf4b1517ee3fac67a43252debac14f8c586
Sander Moolin <sandermoolin@gmail.com>
2021-11-27 20:59:43 -0500

    speckle adjust

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,17 @@ import * as THREE from 'three'
 export const App = defineComponent({
     name: 'App',
     setup() {
+        // mounted
+        // ====================
         onMounted(() => {
             camera.value?.lookAt(new THREE.Vector3(0, 0, 0))
+            ;(camera.value as THREE.OrthographicCamera).zoom = 1.5
         })
 
+        // render function
+        // ====================
         return () => (
-            <lunchbox cameraPosition={[10, 10, 10]} transparent>
+            <lunchbox cameraPosition={[10, 10, 10]} transparent ortho>
                 <Ground />
                 <House />
             </lunchbox>
--- src/Ground/groundFragment.glsl
+++ src/Ground/groundFragment.glsl
@@ -1,8 +1,11 @@
 varying vec2 vUv;
 
 #pragma glslify: voronoi2d = require('glsl-voronoi-noise/2d')
+#pragma glslify: snoise2 = require(glsl-noise/simplex/2d) 
 
 void main(){
-    float str = step(0.02, voronoi2d(vUv * 100.));
+    // float density = 
+    float str = step(0.1, voronoi2d(vUv * 1000.));
+    str = mix(1., str, step(0.7, snoise2(vUv * vec2(100., 200.))));
     gl_FragColor = vec4(vec3(str), 1. - str);
 }
